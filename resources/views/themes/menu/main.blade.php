34ac5d73ad494f37a9676e8e1225038a1e8c6266
Luis Mayta <luis.mayta@gmail.com>
2018-11-04 17:50:28 -0500

    base inicial

---
+++ resources/views/themes/menu/main.blade.php
@@ -0,0 +1,39 @@
+<div class="menu_wrapper">
+    <nav id="menu">
+        <ul id="menu-main-menu" class="menu">
+            <li class="current_page_item">
+                <a href="{{ route('index.home') }}"><span>
+                    <i style="font-size: 80%;" class="fas fa-home"></i> Inicio</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><span>
+                    <i style="font-size: 80%;" class="fas fa-boxes"></i> Material</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><span>
+                    <i style="font-size: 80%;" class="fas fa-feather"></i> Biografía</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><span>
+                    <i style="font-size: 80%;" class="fas fa-calendar-alt"></i> Eventos</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><span>
+                    <i style="font-size: 80%;" class="fab fa-blogger"></i> Blog</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><span>
+                    <i style="font-size: 80%;" class="fas fa-mobile-alt"></i> Contáctame</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <a class="responsive-menu-toggle" href="#">
+        <i class='fas fa-menu'></i>
+    </a>
+</div>
