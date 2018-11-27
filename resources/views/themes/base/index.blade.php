85316eb1ba22a9c3910f3856be8c9faaee74271e
Luis Mayta <luis.mayta@drinux.com>
2018-11-27 17:11:31 -0500

    igualr ancho, chat, recaptcha pedido

--- resources/views/themes/base/index.blade.php
+++ resources/views/themes/base/index.blade.php
@@ -28,6 +28,18 @@
 
     {{-- ReCaptcha --}}
     <script src='https://www.google.com/recaptcha/api.js'></script>
+    {{-- Chat --}}
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = '65420d5f4bd689476082124f8beb065e589c4b29';
+        window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+        })(document);
+    </script>
 
 </head>
 
